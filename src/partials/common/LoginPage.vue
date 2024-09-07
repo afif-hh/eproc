7f42f9612d57584ad213c471dbefbf816201791c
afifnandya <afifnandya@gmail.com>
2024-09-07 17:16:38 +0700

    feat: init

---
+++ src/partials/common/LoginPage.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+</script>
+
+<template>
+  <div class="h-screen">
+    <div class="flex items-center justify-center h-full">
+      <Card class="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle class="text-2xl"> Login </CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account.
+          </CardDescription>
+        </CardHeader>
+        <CardContent class="grid gap-4">
+          <div class="grid gap-2">
+            <Label for="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
+          </div>
+          <div class="grid gap-2">
+            <Label for="password">Password</Label>
+            <Input id="password" type="password" required />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button class="w-full bg-primary"> Sign in </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  </div>
+</template>
